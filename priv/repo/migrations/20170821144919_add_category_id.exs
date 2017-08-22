d6ffa140cf9bbfa4a4d10789ab021e218650a4f8
PeterYzy92 <peteryzy92@gmail.com>
2017-08-22 20:27:00 +0800

    Delete 20170821144919_add_category_id.exs

--- priv/repo/migrations/20170821144919_add_category_id.exs
+++
@@ -1,9 +0,0 @@
-defmodule LibraryApp.Repo.Migrations.AddCategoryId do
-  use Ecto.Migration
-
-  def change do
-  	alter table(:categories) do 
-  		add :book_id, references(:books)
-  	end
-  end
-end
