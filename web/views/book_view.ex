aab786b6383a5fd0dcb1d080c8b9cc70dee27f39
angelinechong728 <angelinechong728@gmail.com>
2017-08-22 22:38:44 +0800

    template designs

--- web/views/book_view.ex
+++ web/views/book_view.ex
@@ -1,3 +1,13 @@
 defmodule LibraryApp.BookView do
   use LibraryApp.Web, :view
+
+  def panel_block do
+  	"<div class='panel panel-primary'>
+  		<div class='panel-heading'>TITLE</div>
+  		<div class='panel-body'>
+		  	 Im a panel
+  		</div>
+
+  	 </div>"
+  end
 end
