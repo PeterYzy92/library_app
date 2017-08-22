8e431652d91950e0a182a595e60330e73d2237e6
PeterYzy <peteryzy92@gmail.com>
2017-08-22 22:32:20 +0800

    templates

---
+++ priv/repo/migrations/20170822132437_addbookid.exs
@@ -0,0 +1,11 @@
+defmodule LibraryApp.Repo.Migrations.Addbookid do
+  use Ecto.Migration
+
+ 
+  	def change do
+  	alter table(:books) do 
+  		add :category_id, references(:categories)
+  	end
+
+  end
+end
